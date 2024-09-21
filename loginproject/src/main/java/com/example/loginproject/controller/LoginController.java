8701cb5b3e86aa1f7d3a06907e0a56e0b6d9306a
gustavocardosorod <gustavocardoso1705@gmail.com>
2024-09-21 09:17:50 -0300

    Login como pagina principal

--- loginproject/src/main/java/com/example/loginproject/controller/LoginController.java
+++ loginproject/src/main/java/com/example/loginproject/controller/LoginController.java
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/login")
+    @GetMapping({ "/", "/login" })
     public String loginForm(Model model) {
         model.addAttribute("user", new User());
         return "login";
