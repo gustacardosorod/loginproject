6a9639bd2ae2151ead18ae3f55eeb6c467cc52a5
gustavocardosorod <gustavocardoso1705@gmail.com>
2024-09-21 09:23:46 -0300

    nova mensagem

--- loginproject/src/main/java/com/example/loginproject/controller/DeveloperController.java
+++ loginproject/src/main/java/com/example/loginproject/controller/DeveloperController.java
@@ -10,7 +10,7 @@ public class DeveloperController {
     @GetMapping("/developer")
     public String developerInfo(Model model) {
         model.addAttribute("name", "Gustavo Cardoso Rodrigues");
-        model.addAttribute("description", "Desenvolvedor de software focado em aplicações Java.");
+        model.addAttribute("description", "Desenvolvedor de software  em aplicações Java.");
         return "developer";
     }
 }
